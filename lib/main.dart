d17b7688aafff38f3f7d999b60764f86d42b1f0b
RyoUchida0415 <tkkndshr2565@gmail.com>
2021-10-04 23:17:17 +0900

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:memooo/pages/top_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,3 +23,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//Todo firebase ios未対応
---
+++ lib/model/memo.dart
@@ -0,0 +1,9 @@
+class Memo{
+  String? title;
+  String? detail;
+  DateTime? createdTime;
+  DateTime? updateTime;
+
+  Memo({this.title,this.detail,this.createdTime,this.updateTime});
+
+}
